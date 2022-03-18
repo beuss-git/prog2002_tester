06c47ea27d5daacee35b7c1756877758e9174600
Beuss <beuss-git@gmail.com>
2022-03-18 05:25:01 +0100

    Added new include directories to test driver

--- tests/RenderdocTest.cmake
+++ tests/RenderdocTest.cmake
@@ -1,7 +1,19 @@
 enable_testing()
 
 
-include_directories(${CMAKE_SOURCE_DIR}/tests)
+include_directories(
+	${CMAKE_SOURCE_DIR}/tests 
+	${CMAKE_SOURCE_DIR}/external
+	${CMAKE_SOURCE_DIR}/external/include
+	${CMAKE_SOURCE_DIR}/external/fmt/include
+)
+
+if (MSVC)
+	add_compile_definitions(RENDERDOC_PLATFORM_WIN32)
+	add_compile_definitions(WIN32)
+else()
+	add_compile_definitions(RENDERDOC_PLATFORM_LINUX)
+endif()
 
 # KIT = TestDriverName
 # KIT_LIBS = Libraries
@@ -14,9 +26,13 @@ function(create_test_driver KIT KIT_LIBS KitTests)
 
 	create_test_sourcelist(Tests ${KIT}TestDriver.cxx ${KitTests})
 
-	add_executable(${KIT}TestDriver ${KIT}TestDriver.cxx ${Tests} ${ADDITIONAL_SRC})
+	add_executable(${KIT}TestDriver ${KIT}TestDriver.cxx ${Tests} ${ADDITIONAL_SRC} "renderdoc_helper.cpp" "renderdoc_helper.hpp")
 
-	target_link_libraries(${KIT}TestDriver LINK_PUBLIC ${KIT_LIBS})
+	target_link_libraries(${KIT}TestDriver LINK_PUBLIC ${KIT_LIBS} )
+	# FIXME
+	target_link_libraries(${KIT}TestDriver LINK_PUBLIC "G:/repos/renderdoc/x64/Release/renderdoc.lib" fmt)
+
+
 	target_compile_features(${KIT}TestDriver PRIVATE cxx_std_17)
 endfunction()
 
