20dd718bbc760554e7d8f8d179cdc413c87fa503
Beuss <beuss-git@gmail.com>
2022-03-18 05:25:18 +0100

    Added POC drawcalls test to test driver

--- tests/renderdoc_test.cpp
+++ tests/renderdoc_test.cpp
@@ -1,58 +1,41 @@
-#include <stdio.h>
-#include <array>
-#include "../external/include/argparse/argparse.hpp"
+#include <argparse/argparse.hpp>
+#include <fmt/core.h>
+#include "renderdoc_helper.hpp"
 
-//std::pair<std::string, int> exec(std::string cmd) {
-//    std::array<char, 128> buffer{};
-//    std::string result;
-//    FILE* pipe = _popen(cmd.c_str(), "r");
-//    if (!pipe) {
-//        throw std::runtime_error("popen() failed!");
-//    }
-//	while (fgets(buffer.data(), buffer.size(), pipe) != nullptr) {
-//        result += buffer.data();
-//    }
-//    int rc = _pclose(pipe);
-//    return std::make_pair(result, rc);
-//}
+
+REPLAY_PROGRAM_MARKER();
+
+bool parse_arguments(argparse::ArgumentParser& parser, int argc, char* argv[]) {
+	parser.add_argument("--drawcalls_count").help("target drawcalls count").scan<'i', int>().default_value(-1);
+
+	try {
+		parser.parse_args(argc, argv);
+	}
+	catch (const std::runtime_error& err) {
+		std::cerr << err.what() << std::endl;
+		std::cerr << parser;
+		return false;
+	}
+	return true;
+}
 
 int renderdoc_test(int argc, char* argv[]) {
-	//printf("renderdoc_test\n");
+	argparse::ArgumentParser parser("test_framework");
+	if (!parse_arguments(parser, argc, argv))
+		return -1;
 
- //   // TODO: hardcoded paths
- //   std::stringstream arguments;
- //   for (int i = 1; i < argc; i++) {
- //       arguments << argv[i] << " ";
- //   }
- //   std::cout << "Arguments: " << arguments.str() << "\n";
+	const auto expected_drawcalls_count = parser.get<int>("drawcalls_count");
 
- //   static const std::string test_script_path = R"("G:\repos\test_framework_tester\run_tests2.py")";
- //   static const std::string python_executable = R"("C:\Python36\python.exe)";
- //   static const std::string pyrenderdoc = R"(--pyrenderdoc "G:\GitProjects\renderdoc-1.18\x64\Release\pymodules")";
- //   static const std::string renderdoc = R"(--renderdoc "G:\GitProjects\renderdoc-1.18\x64\Release")";
- //   static const std::string test_binary = R"(--test-binary "G:\repos\test_framework\out\build\x64-Debug\test_framework.exe")";
+	RenderDocHelper helper(R"(G:\repos\test_framework_tester\tmp\TestFramework\capture_frame5.rdc)");
+	helper.open_capture();
 
-	//static const std::string command =
-	//	python_executable + " "
-	//	+ test_script_path + " "
- //       + arguments.str() + " " // Pass on arguments from this executable to the test script
-	//	+ pyrenderdoc + " "
-	//	+ renderdoc + " "
-	//	+ test_binary;
-
- //   std::cout << "Command: " << command << "\n";
-	//try {
-	//	const auto [output, exit_code] = exec(command);
-
-	//	std::cout << "Output: " << output;
-	//	if (exit_code != 0) {
-	//		return -1;
-	//	}
-	//}
-	//catch (std::runtime_error& e) {
- //       std::cerr << e.what();
- //       return -1;
-	//}
+	if (expected_drawcalls_count != -1) {
+		if (const auto drawcalls_count = helper.drawcalls_count();
+			static_cast<size_t>(expected_drawcalls_count) != drawcalls_count) {
+			fmt::print("found {} drawcalls, expected {}", drawcalls_count, expected_drawcalls_count);
+			return -1;
+		}
+	}
 
 	return 0;
 }
