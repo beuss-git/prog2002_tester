8a751dec631de5f76b0d001e7768a579066bcd8c
Beuss <beuss-git@gmail.com>
2022-03-20 17:52:57 +0100

    Removed test code from lab1 code

--- src/main.cpp
+++ src/main.cpp
@@ -5,27 +5,8 @@
 // Add this to test driver (before)
 
 int main(int argc, char* argv[]) {
-
-#if 0
-	//RenderDoc::Dump renderdoc(R"(G:\repos\test_framework_tester\tmp\TestFramework\capture_frame5.xml)");
-	//if (!renderdoc.parse()) {
-	//	return -1;
-	//}
-	//renderdoc.dump_header();
-	//renderdoc.dump_chunks();
-	//const auto action = renderdoc.find_action("glDrawArrays");
-	//if (action.empty()) {
-	//	return -1;
-	//}
-	// Load reference image to compare
-
-	
-	return 0;
-#else
 	Lab01 lab01(argc, argv);
 	//example.run();
 
 	return lab01.main();
-#endif
-	
 }
