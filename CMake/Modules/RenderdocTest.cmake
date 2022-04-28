b816ee3a110b3aec646df3684fbd5e0c14cd65ea
Benjamin Letnes Bjerken <benjabj@stud.ntnu.no>
2022-04-28 17:58:04 +0200

    Remove old test check code that was moved to TestManager.cmake

--- CMake/Modules/RenderdocTest.cmake
+++ CMake/Modules/RenderdocTest.cmake
@@ -1,3 +1,4 @@
+include(CMake/Modules/TestManager.cmake)
 
 find_library(RENDERDOC_LIBRARY NAMES renderdoc REQUIRED)
 
@@ -36,28 +37,8 @@ function(create_test_driver KIT)
 endfunction()
 
 
-# Sets TEST_ENABLED if match is found in EnabledTests.txt
-function(check_test_enabled)
-	# Read in the file
-	file(READ ${CMAKE_SOURCE_DIR}/Labs/EnabledTests.txt TESTS_INPUT)
-
-	set(TEST_ENABLED OFF PARENT_SCOPE)
-
-	# I have to match line by line because ^ is broken when reading the file like this
-	STRING(REGEX REPLACE "\r?\n" ";" TESTS_INPUT "${TESTS_INPUT}")
-	foreach(line ${TESTS_INPUT})
-		if("${line}" MATCHES "^[\\s]*${TEST_NAME}$")
-			set(TEST_ENABLED ON PARENT_SCOPE)
-			return()
-		endif()
-	endforeach()
-endfunction()
 
 function(add_renderdoc_test TEST_NAME KIT)
-	# Fail if we can't find EnabledTests.txt
-	if(NOT EXISTS ${CMAKE_SOURCE_DIR}/Labs/EnabledTests.txt)
-		message(FATAL_ERROR "File ${CMAKE_SOURCE_DIR}/Labs/EnabledTests.txt not found")
-	endif()
 
 	check_test_enabled()
 	if (NOT ${TEST_ENABLED})
