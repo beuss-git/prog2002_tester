24327b6d9f3d29b9a1e8890e49cb394afa21439f
Benjamin Letnes Bjerken <benjabj@stud.ntnu.no>
2022-04-28 16:03:05 +0200

    RenderdocTest.cmake: Add compatibility with base project

--- modules/RenderdocTest.cmake
+++ modules/RenderdocTest.cmake
@@ -19,15 +19,10 @@ else()
 endif()
 
 # KIT = TestDriverName
-# KIT_LIBS = Libraries
-# KitTests = sourcefile for test(s)
-function(create_test_driver KIT KIT_LIBS KitTests)
-	#set(CMAKE_TESTDRIVER_BEFORE_TESTMAIN "${emscripten_before}#include \"itkTestDriverBeforeTest.inc\"")
-	#set(CMAKE_TESTDRIVER_AFTER_TESTMAIN "#include \"itkTestDriverAfterTest.inc\"${emscripten_after}")
-
+function(create_test_driver KIT)
 	set(ADDITIONAL_SRC ${ARGN} "src/testing/renderdoc_helper.cpp" "src/testing/renderdoc_helper.hpp")
 
-	create_test_sourcelist(Tests ${KIT}TestDriver.cxx ${KitTests})
+	create_test_sourcelist(Tests ${KIT}TestDriver.cxx "src/testing/renderdoc_test.cpp")
 
 	add_executable(${KIT}TestDriver ${KIT}TestDriver.cxx ${Tests} ${ADDITIONAL_SRC})
 
@@ -35,7 +30,7 @@ function(create_test_driver KIT KIT_LIBS KitTests)
 		${CMAKE_CURRENT_SOURCE_DIR}/external
 	)
 
-	target_link_libraries(${KIT}TestDriver PRIVATE ${KIT_LIBS} ${RENDERDOC_LIBRARY} fmt::fmt argparse::argparse)
+	target_link_libraries(${KIT}TestDriver PRIVATE ${RENDERDOC_LIBRARY} fmt::fmt argparse::argparse)
 
 	target_compile_features(${KIT}TestDriver PRIVATE cxx_std_17)
 endfunction()
@@ -58,7 +53,7 @@ function(check_test_enabled)
 	endforeach()
 endfunction()
 
-function(add_renderdoc_test TEST_NAME TARGET_NAME KIT)
+function(add_renderdoc_test TEST_NAME KIT)
 	# Fail if we can't find EnabledTests.txt
 	if(NOT EXISTS ${CMAKE_SOURCE_DIR}/Labs/EnabledTests.txt)
 		message(FATAL_ERROR "File ${CMAKE_SOURCE_DIR}/Labs/EnabledTests.txt not found")
@@ -90,7 +85,7 @@ function(add_renderdoc_test TEST_NAME TARGET_NAME KIT)
 	endif(NOT PARSED_FRAME_COUNT)
 
 	# Path to the executable we want to capture
-	set(EXE_PATH ${PROJECT_BINARY_DIR}/${KIT})
+	set(EXE_PATH ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${KIT})
 
 	# renderdoccmd executable
 	set(CMD_BIN renderdoccmd)
@@ -98,17 +93,19 @@ function(add_renderdoc_test TEST_NAME TARGET_NAME KIT)
 	# arguments to pass on to the executable we want to capture
 	set(CMD_ARGS "capture -w -c ${KIT} ${EXE_PATH} --test --frame ${PARSED_FRAME} --framecount ${PARSED_FRAME_COUNT}")
 
-	set(TESTDRIVER_BIN "${CMAKE_CURRENT_BINARY_DIR}/${KIT}TestDriver")
+	set(TESTDRIVER_BIN "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${KIT}TestDriver")
 
 	# Probably a better way to do this.. but this works
 	string (REPLACE ";" " " PARSED_UNPARSED_ARGUMENTS "${PARSED_UNPARSED_ARGUMENTS}")
 
+	set(TESTDRIVER_TEST_NAME "src/testing/renderdoc_test")
+
 	# Pass on PARSED_UNPARSED_ARGUMENTS to ignore the parsed arguments
 	# we don't want to pass them to the executable
-	set(TESTDRIVER_ARGS "${PARSED_UNPARSED_ARGUMENTS} --capture_file ${PROJECT_BINARY_DIR}/${KIT}_capture.rdc")
+	set(TESTDRIVER_ARGS "${TESTDRIVER_TEST_NAME} ${PARSED_UNPARSED_ARGUMENTS} --capture_file ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${KIT}_capture.rdc")
 
 
-	add_test(NAME RenderDocTest COMMAND 
+	add_test(NAME ${TEST_NAME} COMMAND 
 			${CMAKE_COMMAND}
 				-DCMD1=${CMD_BIN}				# Launches test binary through renderdoccmd
 				-DCMD1ARGS=${CMD_ARGS}
