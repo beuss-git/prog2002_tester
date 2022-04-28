483d39fd717a0cbeca05d08797710ffe80641a6a
Benjamin Letnes Bjerken <benjabj@stud.ntnu.no>
2022-04-28 16:03:16 +0200

    Tests.cmake: Add compatibility with base project

--- modules/Tests.cmake
+++ modules/Tests.cmake
@@ -1,15 +1,12 @@
 include (modules/RenderdocTest.cmake)
 
-# This is 'dynamic' for now, but might end up in all tests
-set(Lab1Tests src/testing/renderdoc_test.cpp)
 
-create_test_driver(Lab1 "${Lab1Tests_LIBRARIES}" "${Lab1Tests}")
+create_test_driver(LabXX)
 
 add_renderdoc_test(
-		Lab00_Test_01	
-		Lab1						# exe
+		LabXX_Test_01	
+		LabXX						# exe
 		FRAME 5						# data for test executable
 		FRAME_COUNT 20
-		renderdoc_test				# arguments to Test Driver, the first argument here can probably be hardcoded in the function itself
-		--drawcalls_count 3
+		--drawcalls_count 3			# arguments to Test Driver, the first argument here can probably be hardcoded in the function itself
 )
