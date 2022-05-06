45272b6f89904cd19c3eddff571bbcbf9121ed86
Benjamin Letnes Bjerken <benjabj@stud.ntnu.no>
2022-05-06 03:07:35 +0200

    Add new tests to Tests.cmake

--- CMake/Modules/Tests.cmake
+++ CMake/Modules/Tests.cmake
@@ -10,12 +10,38 @@ check_enabledtests_exists()
 ]]
 create_test_driver(LabXX)
 
-add_renderdoc_test(
+#[[add_renderdoc_test(
 		LabXX_Test_01
 		LabXX						# exe
 		FRAME 5						# data for test executable
 		FRAME_COUNT 20
 		--drawcalls_count 3			# arguments to Test Driver, the first argument here can probably be hardcoded in the function itself
+)]]
+
+add_renderdoc_test(
+		LabXX_Test_02
+		LabXX						
+		FRAME 1
+		FRAME_COUNT 1
+		--vao_count 1
+		--vbo_count 1
+		--vbo_transfer_count 1
+)
+
+add_renderdoc_test(
+		LabXX_Test_03
+		LabXX						
+		FRAME 1
+		FRAME_COUNT 1
+		--shader_count 2
+)
+
+add_renderdoc_test(
+		LabXX_Test_04
+		LabXX						
+		FRAME 1
+		FRAME_COUNT 1
+		--drawcalls_count 1
 )
 
 
